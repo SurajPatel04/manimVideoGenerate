f5e8cad3a0975a2d1b4a5d55145372106defa6eb
SurajPatel04 <surajpatel9390@gmail.com>
2025-07-24 00:47:11 +0530

    schema update

--- backend/schema.py
+++ backend/schema.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 from typing import Optional
 
-class State(BaseModel):
+
+# descriptionGenerate Schema
+class DescriptionGenerationState(BaseModel):
     user_query:str
     descriptions:list[str] = []
     pickedOne: str
@@ -18,3 +20,19 @@ class PickOneDescription(BaseModel):
 class CheckPickedDescription(BaseModel):
     is_this_good_descrription: bool
     pickedOneError: str
+
+
+# manimCodeGeneration Schema
+class CheckMaimCode(BaseModel):
+    is_code_good: bool
+    error_message: str
+
+
+class mainmState(BaseModel):
+    description: str
+    is_code_good: Optional[bool] = None
+    filename: str
+    error_message: Optional[str] = None
+    rewrite_attempts: int = 0 
+    execution_success: Optional[bool] = None
+    quality: str = "ql"
