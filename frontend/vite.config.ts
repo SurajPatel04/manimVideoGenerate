3e1e5a9e775e19d1fc41188b5e1cf3aa789e2e23
SurajPatel <surajpatel9390@gmail.com>
2025-09-12 13:24:31 +0530

    update: gitignore and vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
@@ -11,4 +11,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8000/',
+          changeOrigin: true,
+          secure: true,
+          // Remove /api prefix if your backend doesn’t use it
+          // rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
 })
